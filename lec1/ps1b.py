63b41cb84fceb726d756624fe06d9f253828b63e
keivalya <keivalyapandya2001@gmail.com>
2024-12-28 17:37:06 -0500

    final ps1b.py

---
+++ lec1/ps1b.py
@@ -0,0 +1,24 @@
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = int(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
+
+portion_down_payment = 0.25
+current_savings = 0
+additional_return = 0
+
+monthly_salary = annual_salary / 12
+amount_saved = portion_saved * monthly_salary
+amount_down_payment = portion_down_payment * total_cost
+
+r = 0.04
+months = 0
+
+while current_savings < amount_down_payment:
+    months += 1
+    if months % 6 == 0:
+        monthly_salary += monthly_salary * semi_annual_raise
+    current_savings += (current_savings * r / 12) + (portion_saved * monthly_salary)
+    print(f"Month: {months}, Current savings: {current_savings}, Additional return: {additional_return}")
+
+print(f"Number of months: {months}")
