e890faf6c77920156a5f33bd8ae43c87a0ada210
keivalya <keivalyapandya2001@gmail.com>
2024-12-28 17:29:53 -0500

    final ps1a.py

--- lec1/ps1a.py
+++ lec1/ps1a.py
@@ -1,25 +1,21 @@
-# annual_salary = int(input("Enter your annual salary: "))
-# portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
-# total_cost = int(input("Enter the cost of your dream home: "))
-
-annual_salary = 120000
-portion_saved = .10
-total_cost = 1000000
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = int(input("Enter the cost of your dream home: "))
 
 portion_down_payment = 0.25
 current_savings = 0
 additional_return = 0
 
-amount_saved = portion_saved * annual_salary / 12
+monthly_salary = annual_salary / 12
+amount_saved = portion_saved * monthly_salary
 amount_down_payment = portion_down_payment * total_cost
 
 r = 0.04
-months = 1
+months = 0
 
 while current_savings < amount_down_payment:
-    additional_return += current_savings * r / 12
-    amount_saved += amount_saved / 12
-    current_savings += additional_return + amount_saved
+    current_savings += (current_savings * r / 12) + amount_saved
     months += 1
+    print(f"Month: {months}, Current savings: {current_savings}, Additional return: {additional_return}")
 
 print(f"Number of months: {months}")
