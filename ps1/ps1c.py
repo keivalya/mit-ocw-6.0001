1a95b67df970dfe275dc67812dbcd3881bb913d9
keivalya <keivalyapandya2001@gmail.com>
2024-12-28 19:33:07 -0500

    updated ps1c and added lec2

--- ps1/ps1c.py
+++ ps1/ps1c.py
@@ -1,23 +1,36 @@
 starting_salary = int(input("Enter the starting salary: "))
 
+epsilon = 100
 semi_annual_raise = 0.07
 total_cost = 1000000
 portion_down_payment = 0.25
 current_savings = 0
 additional_return = 0
 
-monthly_salary = annual_salary / 12
-amount_saved = portion_saved * monthly_salary
 amount_down_payment = portion_down_payment * total_cost
 
 r = 0.04
 months = 0
 
-while current_savings < amount_down_payment:
-    months += 1
-    if months % 6 == 0:
-        monthly_salary += monthly_salary * semi_annual_raise
-    current_savings += (current_savings * r / 12) + (portion_saved * monthly_salary)
-    print(f"Month: {months}, Current savings: {current_savings}, Additional return: {additional_return}")
+high, low = 10000, 0
+guess_portion_saved = (high + low) // 2
+num_guesses = 0
 
-print(f"Number of months: {months}")
+while abs(current_savings - amount_down_payment) > epsilon:
+    current_savings = 0
+    monthly_salary = starting_salary / 12
+    guess_portion_saved = (high + low) // 2
+    for i in range(36):
+        current_savings += (current_savings * r / 12) + (guess_portion_saved/10000 * monthly_salary)
+        if i % 6 == 0:
+            monthly_salary += monthly_salary * semi_annual_raise
+    if current_savings < amount_down_payment:
+        low = guess_portion_saved
+    else:
+        high = guess_portion_saved
+    if high-1 == low: print("It is not possible to pay the down payment in three years."); exit()
+    print(f"Current savings: {current_savings}, High: {high}, Low: {low}")
+    num_guesses += 1
+
+print(f"Best savings rate: {guess_portion_saved / 10000}")
+print(f"Steps in bisection search: {num_guesses}")
